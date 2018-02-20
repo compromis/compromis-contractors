30acd979494dcc0bb6b45f5cd476b6cafd85e517
Guillem <guillem@disedit.com>
2018-02-20 17:13:58 +0100

    Basic set up

---
+++ src/components/Table.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <vuetable ref="vuetable"
+      api-url="https://vuetable.ratiw.net/api/users"
+      :fields="fields">
+    </vuetable>
+    <vuetable-pagination ref="pagination"></vuetable-pagination>
+  </div>
+</template>
+
+<script>
+import Vuetable from 'vuetable-2/src/components/Vuetable'
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    Vuetable,
+    VuetablePagination
+  },
+  data () {
+    return {
+      fields: ['name', 'email', 'birthdate']
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
