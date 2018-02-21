f789e6383a868923a879a82dd258392e4bd90bbb
Guillem <guillem@disedit.com>
2018-02-21 15:45:31 +0100

    Add loading and empty messages

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,7 @@ export default {
 </script>
 
 <style>
-
+  #app {
+    font-family: Compromis, sans-serif;
+  }
 </style>
--- src/components/Open.vue
+++ src/components/Open.vue
@@ -2,11 +2,11 @@
   <div>
     <h2>Anuncis de licitació</h2>
 
-    <div v-if="loading">
+    <div v-if="loading" class="placeholder">
       Carregant...
     </div>
-    <div v-else-if="bids.length == 0">
-      No hi ha cap...
+    <div v-else-if="bids.length == 0" class="placeholder">
+      No hi ha cap anunci de licitació obert
     </div>
     <div v-else>
       <open-bid v-for="bid in bids" :key="bid.id" :bid="bid" />
@@ -49,4 +49,18 @@ export default {
 </script>
 
 <style lang="scss">
+  @import '../variables';
+
+  .placeholder {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: $default-border;
+    color: $gray-text;
+    font-size: 2rem;
+    text-align: center;
+    padding: 2rem;
+    margin: 2rem 0;
+    min-height: 124px;
+  }
 </style>
